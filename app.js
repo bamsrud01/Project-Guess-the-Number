e616478d4b23428a880220e6e989affc3e6d2a1d
Barrett Amsrud <barrettamsrud@gmail.com>
2016-10-03 16:09:42 -0500

    Wrote framework for app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//Create a random number function, with minimum and maximum settings
+
+//Have the user choose both numbers
+  //Filter out non-number entries
+
+//Create a counter for the number of turns remaining
+
+//Write a loop for the user to guess the number
+  //Check to see if the number is correct
+  //Inform the user if the number is higher or lower
+  //Track the turns remaining
+
+//After completion, inform the player of the correct number and total turns used
+
+//Ask the player to play again.
